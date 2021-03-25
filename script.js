998d9e5343a4ee7c99f8e7f774a19d2bde2f8021
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-25 14:31:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let age = 64
+let guess = 0
+
+while (guess != age) {
+  age=parseInt(age)
+  guess=parseInt(guess)
+  guess = prompt('guess my age(between 0 and 100)')
+  if (guess > age) prompt('your guess is too high')
+  else if (guess < age) prompt('your guess is too low')
+}
+if (guess === age) alert('you win!')
