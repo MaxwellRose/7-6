8da88c5704372cbdeb5bed54419a34c9ed2c5ab0
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-25 15:01:24 +0000

    lol

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let age = 64
 let guess = 0
 
 while (guess !== age) {
-  age= parseInt(age)
-  guess = parseInt(guess);
+  age = parseInt(age)
+  guess = parseInt(guess)
   guess = prompt('guess my age(between 0 and 100)')
   if (guess > age) prompt('your guess is too high')
   else if (guess < age) prompt('your guess is too low')
